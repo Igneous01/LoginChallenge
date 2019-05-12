9cf223895e43693079819847d4ab99b029827c52
Igneous01 <5571589+Igneous01@users.noreply.github.com>
2019-05-12 18:38:13 -0400

    Project Added
    Took example project generated from ASP.NET Core Web Application and reduced to bare bones based on task description.

---
+++ LoginAssignment/Controllers/StatusController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using LoginAssignment.Models;
+
+namespace LoginAssignment.Controllers
+{
+    public class StatusController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public StatusController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                ApplicationUser currentUser = await _userManager.GetUserAsync(User);
+                if (currentUser != null)       
+                    return View(new CurrentUserModel { FirstName = currentUser.FirstName, LastName = currentUser.LastName });
+                else
+                    return View();
+            }            
+            else
+                return new RedirectToActionResult("Login", "Account", new { returnUrl = "~/Status/Index", isRedirect = true });
+        }
+    }
+}
---
+++ LoginAssignment/Models/CurrentUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAssignment.Models
+{
+    public class CurrentUserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
